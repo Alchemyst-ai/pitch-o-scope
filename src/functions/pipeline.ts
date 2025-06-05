5073b4db49cfd0fbc7f6cde52ab8d3da58f3f6cf
ANIR1604 <competitivecoder69@gmail.com>
2025-06-05 12:42:35 +0530

    Completed

--- src/functions/pipeline.ts
+++ src/functions/pipeline.ts
@@ -37,7 +37,7 @@ export async function processLeadsPipeline({
 }): Promise<Lead[]> {
   const leadsWithIds = ensureUniqueIds(leads);
 
-  const batches = batchArray(leadsWithIds, 50);
+  const batches = batchArray(leadsWithIds, 100);
 
   let groups: Group[] = [];
   const leadToGroup: Record<string, string> = {};
